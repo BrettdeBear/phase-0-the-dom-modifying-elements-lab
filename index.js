602a6ecc9853fdcd922622d083a482835c6c2a38
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-11 16:29:31 -0700

    did itgit add .

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your code here!
 
 const main = document.getElementById("main");
-    main.remove();
+main.remove();
 
 const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Brett is the champion";
